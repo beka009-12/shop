18510136a7b4f56acb0e16bac59328ceb9f90f01
beka009-12 <bekbolnurmsmitov220099@gmail.com>
2026-02-05 18:10:08 +0600

    .

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API}/nest-shop`,
+  baseURL: `https://express-production-2192.up.railway.app/nest-shop`,
 });
 
 api.interceptors.request.use((config) => {
